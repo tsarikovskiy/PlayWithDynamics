209de2bdf665160305c33b8af9ee10aa95dd7884
Serg Tsarikovskiy <sergey.tsarikovskiy@appflame.com>
2020-03-22 16:00:51 +0200

    privki

--- PlayWithDynamics/ViewController.swift
+++ PlayWithDynamics/ViewController.swift
@@ -9,23 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    // Privetiki
     @IBOutlet private weak var containerView: UIView!
     private var animator: UIDynamicAnimator!
     private var gravity: UIGravityBehavior!
     private var collision: UICollisionBehavior!
     private var bounce: UIDynamicItemBehavior!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+
         let circleView = BallView(frame: .init(x: 150, y: 700, width: 180, height: 180))
         circleView.backgroundColor = .yellow
         circleView.layer.masksToBounds = true
         circleView.layer.cornerRadius = 90
         containerView.addSubview(circleView)
-        
+
         let circleView2 = BallView(frame: .init(x: UIScreen.main.bounds.width / 2,
                                                 y: 350,
                                                 width: 120, height: 120))
@@ -33,13 +33,13 @@ class ViewController: UIViewController {
         circleView2.layer.masksToBounds = true
         circleView2.layer.cornerRadius = 60
         containerView.addSubview(circleView2)
-        
+
         let circleView3 = BallView(frame: .init(x: 0, y: 0, width: 92, height: 92))
         circleView3.backgroundColor = .purple
         circleView3.layer.masksToBounds = true
         circleView3.layer.cornerRadius = 46
         containerView.addSubview(circleView3)
-        
+
         let circleView4 = BallView(frame: .init(x: 50, y: 0, width: 180, height: 180))
         circleView4.backgroundColor = .yellow
         circleView4.layer.masksToBounds = true
@@ -59,18 +59,18 @@ class ViewController: UIViewController {
         circleView6.layer.masksToBounds = true
         circleView6.layer.cornerRadius = 46
         containerView.addSubview(circleView6)
-        
+
         animator = UIDynamicAnimator(referenceView: containerView)
         gravity = UIGravityBehavior(items: [circleView, circleView2, circleView3, circleView4, circleView5, circleView6])
-        
+
         gravity.magnitude = 0.8
         animator.addBehavior(gravity)
-        
+
         collision = UICollisionBehavior(items: [circleView, circleView2, circleView3, circleView4, circleView5, circleView6])
         collision.collisionMode = .everything
         collision.translatesReferenceBoundsIntoBoundary = true
         animator.addBehavior(collision)
-        
+
         bounce = UIDynamicItemBehavior(items: [circleView, circleView2, circleView3, circleView4, circleView5, circleView6])
 //        bounce.addAngularVelocity(5, for: [circleView])
         bounce.elasticity = 0.8
@@ -78,11 +78,11 @@ class ViewController: UIViewController {
 //        bounce.density = 0.3
 //        bounce.friction = 0.1
 //        bounce.charge =
-        
+
         animator.addBehavior(bounce)
-        
-        
-        
+
+
+
     }
 }
 
@@ -92,4 +92,3 @@ final class BallView : UIView {
         return .ellipse
     }
 }
-
